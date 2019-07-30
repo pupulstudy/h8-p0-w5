ead5d851f02a0a7dffc785012058a8aae40c69e5
pupulstudy <47462483+pupulstudy@users.noreply.github.com>
2019-07-30 12:48:53 +0700

    Update exercise-17.js

--- exercise-17.js
+++ exercise-17.js
@@ -1,4 +1,15 @@
-const kaliTerusRekursif = angka => angka.toString().length > 1 ?  kaliTerusRekursif(parseInt(angka.toString()[angka.toString().length-1]) * kaliTerusRekursif(Math.floor(angka/10))) : angka;
+// const kaliTerusRekursif = angka => angka.toString().length > 1 ?  kaliTerusRekursif(parseInt(angka.toString()[angka.toString().length-1]) * kaliTerusRekursif(Math.floor(angka/10))) : angka;
+
+const kaliTerusRekursif = angka => {
+	if (angka.toString().length > 1) {
+		for (var i = 0, result = 1; i < angka.toString().length; i++) {
+			result *= parseInt(angka.toString()[i]);
+		}
+		return kaliTerusRekursif(result);
+	} else {
+		return angka;
+	}
+}
 
 // TEST CASES
 console.log(kaliTerusRekursif(66)); // 8
@@ -6,3 +17,4 @@ console.log(kaliTerusRekursif(3)); // 3
 console.log(kaliTerusRekursif(24)); // 8
 console.log(kaliTerusRekursif(654)); // 0
 console.log(kaliTerusRekursif(1231)); // 6
+console.log(kaliTerusRekursif(3333)); // 8
